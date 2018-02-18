4da36aaf08e9db13e759ce2e547d8684198a07fc
Ben Marshall <bm12656@my.bristol.ac.uk>
2018-02-18 12:23:36 +0000

    More work on the VCD data structures.
    - Ready to start constructing VCDFile instance using the parser.
    
     On branch master
     Changes to be committed:
    	modified:   VCDFile.hpp
    	modified:   VCDParser.ypp
    	modified:   VCDTypes.hpp

--- src/vcd/VCDFile.hpp
+++ src/vcd/VCDFile.hpp
@@ -15,13 +15,41 @@
 class VCDFile {
 
     public:
+        
+        //! Instance a new VCD file container.
+        VCDFile();
 
+        /*!
+        @brief Add a new scope object to the VCD file
+        */
+        void add_scope(
+            VCDScope * s
+        );
     
+
+        /*!
+        @brief Return the scope object in the VCD file with this name
+        */
+        VCDScope * get_scope(
+            VCDScopeName name
+        );
+        
+        //! Date string of the VCD file.
+        std::string date;
+
+        //! Version string of the simulator which generated the VCD.
+        std::string version;
 
     protected:
 
-
-
+        //! Root scope node of the VCD signals
+        VCDScope * root_scope;
+        
+        //! Flat mao of all scope objects in the file, keyed by name.
+        std::map<VCDScopeName,VCDScope*> scopes;
+        
+        //! Flat vector of all signals in the file.
+        std::vector<VCDSignal*> signals;
 };
 
 
--- src/vcd/VCDTypes.hpp
+++ src/vcd/VCDTypes.hpp
@@ -20,6 +20,42 @@ typedef std::string VCDScopeName;
 //! Compressed hash representation of a signal.
 typedef std::string VCDSignalHash;
 
+//! Represents a single instant in time in a trace
+typedef double VCDTime;
+
+//! Variable types of a signal in a VCD file.
+typedef enum {
+    VCD_VAR_EVENT,
+    VCD_VAR_INTEGER,
+    VCD_VAR_PARAMETER,
+    VCD_VAR_REAL,
+    VCD_VAR_REALTIME,
+    VCD_VAR_REG,
+    VCD_VAR_SUPPLY0,
+    VCD_VAR_SUPPLY1,
+    VCD_VAR_TIME,
+    VCD_VAR_TRI,
+    VCD_VAR_TRIAND,
+    VCD_VAR_TRIOR,
+    VCD_VAR_TRIREG,
+    VCD_VAR_TRI0,
+    VCD_VAR_TRI1,
+    VCD_VAR_WAND,
+    VCD_VAR_WIRE,
+    VCD_VAR_WOR
+} VCDVarType;
+
+
+//! Represents the possible time units a VCD file is specified in.
+typedef enum {
+    TIME_S,     //!< Seconds
+    TIME_MS,    //!< Milliseconds
+    TIME_US,    //!< Microseconds
+    TIME_NS,    //!< Nanoseconds
+    TIME_PS,    //!< Picoseconds
+} VCDTimeUnit;
+
+
 //! Represents the type of SV construct who's scope we are in.
 typedef enum {
     VCD_SCOPE_BEGIN,
@@ -33,12 +69,21 @@ typedef enum {
 // Typedef over vcdscope.
 typedef struct vcdscope VCDScope;
 
+//! Represents a single signal reference within a VCD file
+typedef struct {
+    VCDSignalHash       hash;
+    VCDSignalReference  reference;
+    VCDScope          * scope;
+} VCDSignal;
+
+
 //! Represents a scope type, scope name pair.
 struct vcdscope {
-    VCDScopeName              name;
-    VCDScopeType              type;
-    VCDScope                * parent;
-    std::vector<VCDScope*>    children;
+    VCDScopeName              name;     //!< The short name of the scope
+    VCDScopeType              type;     //!< Construct type
+    VCDScope                * parent;   //!< Parent scope object
+    std::vector<VCDScope*>    children; //!< Child scope objects.
+    std::vector<VCDSignal*>   signals;  //!< Signals in this scope.
 };
 
 
