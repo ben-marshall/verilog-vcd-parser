a6585bd207c121083f4b213ce8c79415c5a949ae
Udi Finkelstein <github@udifink.com>
2019-02-22 04:47:14 +0200

    Travere scopes recursively and display full instance path names.

--- src/VCDFileParser.cpp
+++ src/VCDFileParser.cpp
@@ -27,7 +27,7 @@ VCDFile * VCDFileParser::parse_file(const std::string &filepath) {
     VCDFile * tr = this -> fh;
 
     this -> fh -> root_scope = new VCDScope;
-    this -> fh -> root_scope -> name = std::string("$root");
+    this -> fh -> root_scope -> name = std::string("");
     this -> fh -> root_scope -> type = VCD_SCOPE_ROOT;
     
     this -> scopes.push(this -> fh -> root_scope);
@@ -70,6 +70,38 @@ void VCDFileParser::error(const std::string & m){
 
 #ifdef VCD_PARSER_STANDALONE
 
+void print_scope_signals(VCDScope * scope)
+{
+    for(VCDSignal * signal : scope -> signals) {
+
+        std::cout << "\t" << signal -> hash << "\t" 
+                    << signal -> reference;
+
+        if(signal -> size > 1) {
+            std::cout << "[" << signal -> lindex << ":" << signal -> rindex << "]";
+        } else if (signal -> lindex >= 0) {
+            std::cout << "[" << signal -> lindex << "]";
+        }
+        
+        std::cout << std::endl;
+
+    }
+}
+
+void traverse_scope(std::string parent, VCDScope * scope)
+{
+    std::string local_parent = parent;
+
+    if (parent.length())
+        local_parent += ".";
+    local_parent += scope->name;
+    std::cout << "Scope: " << local_parent  << std::endl;
+    print_scope_signals(scope);
+    for (auto child : scope->children) {
+        std::cout << "Child:\n";
+        traverse_scope(local_parent, child);
+    }
+}
 /*!
 @brief Standalone test function to allow testing of the VCD file parser.
 */
@@ -92,23 +124,9 @@ int main (int argc, char** argv){
     
         // Print out every signal in every scope.
         for(VCDScope * scope : *trace -> get_scopes()) {
-
-            std::cout << "Scope: "  << scope ->  name  << std::endl;
-
-            for(VCDSignal * signal : scope -> signals) {
-
-                std::cout << "\t" << signal -> hash << "\t" 
-                          << signal -> reference;
-
-                if(signal -> size > 1) {
-                    std::cout << "[" << signal -> lindex << ":" << signal -> rindex << "]";
-                } else if (signal -> lindex >= 0) {
-                    std::cout << "[" << signal -> lindex << "]";
-                }
-                
-                std::cout << std::endl;
-
-            }
+            if (scope->parent)
+                continue;
+            traverse_scope(std::string(""), scope);
         }
 
         delete trace;
